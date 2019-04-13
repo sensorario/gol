28472c1f59e48f13f9f311585c13dd390aa4942b
Simone Gentili <sensorario@gmail.com>
2019-04-13 14:56:35 +0200

    use testify assertions

--- gol_test.go
+++ gol_test.go
@@ -1,19 +1,19 @@
 package gol
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestConstructor(t *testing.T) {
 	l := NewLogger("foo")
 	path := l.Path()
-	if path != "/var/log/foo/logger.log" {
-		t.Errorf("Oops! Not expected path!")
-	}
+	assert.Equal(t, "/var/log/foo/logger.log", path, "Oops! Not expected path!")
 }
 
 func TestCustomLoggerPath(t *testing.T) {
 	l := NewCustomLogger("foo/bar")
 	path := l.Path()
-	if path != "foo/bar/logger.log" {
-		t.Errorf("Oops! Not expected path!")
-	}
+	assert.Equal(t, "foo/bar/logger.log", path, "Oops! Not expected path!")
 }
